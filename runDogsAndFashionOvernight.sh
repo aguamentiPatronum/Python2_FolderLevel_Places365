d46678406a89a1ee4417b39ead063ce4a40cac5d
Britt Davis <brittdavis@ByrdPro.local>
2022-01-06 16:02:57 -0800

    after successful overnight run

--- runDogsAndFashionOvernight.sh
+++ runDogsAndFashionOvernight.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-time python run_placesCNN_unified_WholeFolder.py --saveToFile --failureName failedLandcoverFiles --dbName ./LandcoverSceneClassification --input_images /Users/davi784/ProjectRepos/Explanations/Datasets/LandcoverAI/landcover.ai.v1/images/
+time python run_placesCNN_unified_WholeFolder.py --saveToFile --failureName failedLandcoverFiles --dbName ./LandcoverSceneClassification --input_images /Users/brittdavis/Documents/2022_Work/Datasets/LandcoverAI/landcover.ai.v1/images/
 
-time python run_placesCNN_unified_WholeFolder.py --saveToFile --failureName failedDogsFiles --dbName ./StanfordDogsSceneClassification --input_images /Users/davi784/ProjectRepos/Explanations/Datasets/StanfordDogs
+time python run_placesCNN_unified_WholeFolder.py --saveToFile --failureName failedDogsFiles --dbName ./StanfordDogsSceneClassification --input_images /Users/brittdavis/Documents/2022_Work/Datasets/StanfordDogs/
 
-time python run_placesCNN_unified_WholeFolder.py --saveToFile --failureName failedFashionFiles --dbName ./FashionPediaSceneClassification --input_images /Users/davi784/ProjectRepos/Explanations/Datasets/FashionPedia
+time python run_placesCNN_unified_WholeFolder.py --saveToFile --failureName failedFashionFiles --dbName ./FashionPediaSceneClassification --input_images /Users/brittdavis/Documents/2022_Work/Datasets/FashionPedia/
